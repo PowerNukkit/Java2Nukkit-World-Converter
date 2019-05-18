b16d23e785f5de40d1fdbbfa719c9bab5510780d
joserobjr <jose.rob.jr@gmail.com>
2019-05-18 16:36:28 -0300

    Implements conversion from item frame entity to bedrock's item frame tile entity

--- src/main/kotlin/br/com/gamemods/nbtmanipulator/chunk-converter.kt
+++ src/main/kotlin/br/com/gamemods/nbtmanipulator/chunk-converter.kt
@@ -20,7 +20,7 @@ fun Chunk.toNukkit(): NukkitChunk {
         .mapNotNull { it.key to (it.value.toNukkit(javaTileEntities, nukkitTileEntities) ?: return@mapNotNull null) }
         .toMap()
     val nukkitChunk = NukkitChunk(
-        NbtList(javaChunk.entities.value.map { toNukkitEntity(it) }),
+        NbtList(javaChunk.entities.value.mapNotNull { toNukkitEntity(it, javaChunk, nukkitSections, nukkitTileEntities) }),
         nukkitSections,
         NbtList(nukkitTileEntities.values.toMutableList()),
         javaChunk.inhabitedTime,
@@ -112,8 +112,65 @@ fun toNukkitTileEntity(javaEntity: NbtCompound): NbtCompound? {
     return null
 }
 
-fun toNukkitEntity(javaEntity: NbtCompound): NbtCompound {
-    return javaEntity
+fun toNukkitEntity(
+    javaEntity: NbtCompound,
+    javaChunk: JavaChunk,
+    nukkitSections: Map<Int, NukkitChunkSection>,
+    nukkitTileEntities: MutableMap<BlockPos, NbtCompound>
+): NbtCompound? {
+    return when(javaEntity.getString("id").removePrefix("minecraft:")) {
+        "item_frame" -> {
+            val tileX = javaEntity.getInt("TileX")
+            val tileY = javaEntity.getInt("TileY")
+            val tileZ = javaEntity.getInt("TileZ")
+            val chunkSectionY = tileY / 16
+            val chunkSection = nukkitSections[chunkSectionY] ?: return null
+            val internalX = tileX % 32
+            val internalY = tileY % 32
+            val internalZ = tileZ % 32
+            val offset = ((internalY and 0xF) shl 8) + ((internalZ and 0xF) shl 4) + (internalX and 0xF)
+            val blockIdInPosition = chunkSection.blocks[offset].toInt()
+            // Item Frame in Bedrock Edition is a block and not an entity, check if we aren't replacing any block.
+            if (blockIdInPosition != 0) {
+                return null
+            }
+            val facing = when (javaEntity.getByte("Facing").toInt()) {
+                0, 1 -> return null // Facing down and up is unsupported by Bedrock Edition
+                2 -> 3
+                3 -> 2
+                4 -> 5
+                5 -> 4
+                else -> 3
+            }
+            chunkSection.blocks[offset] = (199 and 0xFF).toByte()
+            val halfOffset = offset / 2
+            val isSecond = offset % 2 == 1
+            val stored = if (!isSecond) {
+                facing to (chunkSection.blockData[halfOffset].toInt() and 0x0F)
+            } else {
+                ((chunkSection.blockData[halfOffset].toInt() and 0xF0) shr 4) to facing
+            }
+            val first = stored.first and 0x0F
+            val second = (stored.second and 0x0F) shl 4
+            val merged = first or second
+            chunkSection.blockData[halfOffset] = (merged and 0xFF).toByte()
+            val tileEntity = NbtCompound(
+                "id" to NbtString("ItemFrame"),
+                "x" to NbtInt(tileX),
+                "y" to NbtInt(tileY),
+                "z" to NbtInt(tileZ),
+                "isMoveable" to NbtByte(false)
+            )
+            tileEntity.copyFrom(javaEntity, "ItemDropChance")
+            tileEntity.copyFrom(javaEntity, "ItemRotation")
+            javaEntity.getNullableCompound("Item")?.toNukkitItem()?.let {
+                tileEntity["Item"] = it
+            }
+            nukkitTileEntities[BlockPos(tileX, tileY, tileZ)] = tileEntity
+            null
+        }
+        else -> null
+    }
 }
 
 fun toNukkitBiomes(biomes: IntArray): ByteArray {
